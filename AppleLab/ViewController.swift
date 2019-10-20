97f8239b92b4b05527777c7fb7e6ddb5707f3138
Pedro Belfort <belfortdev@gmail.com>
2019-10-20 15:48:08 +0100

    iOS initial setup

---
+++ AppleLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleLab
+//
+//  Created by Pedro Fernandes on 10/20/19.
+//  Copyright © 2019 Pedro Belfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
